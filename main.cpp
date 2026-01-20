2ba6af314b5773564c20b21849d064b49b7c11f6
jamesmbdevelop <jamesmbdevelop@gmail.com>
2026-01-20 06:30:41 -0500

    project0 first cpp programs

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Hello, World!\n" << std::endl;
+   
+    std::cout << "When you see someone \nwearing the big block \"M\" \nsay \"Go Blue!\"\n" << std::endl;
+
+    std::cout << "Now for a cheer they are here, triumphant!\nHere they come with banners flying,\nIn stalwart step they\'re nighing,\nWith shouts of vict\'ry crying,\nWe hurrah, hurrah, we greet you now,\nHail!\n\nFar we their praises sing\nFor the glory and fame they\'ve bro\'t us\nLoud let the bells them ring\nFor here they come with banners flying\nFar we their praises tell\nFor the glory and fame they\'ve bro\'t us\nLoud let the bells them ring\nFor here they come with banners flying\nHere they come, Hurrah!\n\nHail! to the victors valiant\nHail! to the conqu\'ring heroes\nHail! Hail! to Michigan,\nthe leaders and best\n\nHail! to the victors valiant\nHail! to the conqu\'ring heroes\nHail! Hail! to Michigan,\nthe champions of the West!\n" << std::endl;
+
+ return 0;
+}
